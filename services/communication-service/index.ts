5730f2dadaf3fc7f06d040cdff8db6075f31b61b
UnknownCoding <aaqibakeel75@gmail.com>
2025-07-09 19:56:15 +0400

    v1 of socket io/webrtc for the commservice

--- services/communication-service/index.ts
+++ services/communication-service/index.ts
@@ -2,15 +2,27 @@ import express from "express"
 import cors from "cors"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
+import {Server, Socket} from "socket.io"
+import http from "http"
 
 dotenv.config({
     path:"../../.env"
 })
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server,{
+    cors:{
+        origin:process.env.ORIGIN,
+        methods:["GET","POST"],
+        credentials:true
+    }
+})
+
 app.use(cors({
     origin:process.env.ORIGIN,
-    credentials:true
+    methods:["GET","POST"],
+    credentials:true,
 }))
 
 app.use(express.json({
@@ -21,6 +33,69 @@ app.use(cookieParser())
 
 const PORT = process.env.PORT_COMM || 6000
 
-app.listen(PORT,()=>{
+const waitingUsers:string[] = [];
+
+io.on("connection",(socket:Socket) => {
+    console.log(`User connected ${socket.id}`);
+
+    socket.on("find_partner",() => {
+        console.log(`User ${socket.id} is looking for partner`);
+
+        if(waitingUsers.length === 0){
+            waitingUsers.push(socket.id);
+            socket.emit("waiting")
+        }else{
+            const partnerId = waitingUsers.shift(); 
+            if(partnerId){
+                const roomId = `room_${partnerId}_${socket.id}`
+                socket.join(roomId);
+                const partnerSocket = io.sockets.sockets.get(partnerId);
+                
+                if(partnerSocket){
+                    partnerSocket.join(roomId)
+                    io.to(roomId).emit("partner_found",{
+                        roomId,partnerId,yourId:socket.id
+                    })
+                    console.log(`Paired ${socket.id} with ${partnerId} in room ${roomId}`);
+                }else{
+                    waitingUsers.unshift(socket.id);
+                }
+            }
+        }
+
+    })
+
+
+    socket.on("offer",({roomId,offer}) => {
+        socket.to(roomId).emit("offer",{offer,from:socket.id})
+    })
+
+    socket.on("answer",({roomId,answer})=> {
+        socket.to(roomId).emit("answer",{answer,from:socket.id})
+    })
+
+    socket.on("ice-candidates",({roomId,candidate})=> {
+        socket.to(roomId).emit("ice-candidates",{candidate,from:socket.id})
+    })
+
+    socket.on("disconnect",()=>{
+        console.log(`User disconnected ${socket.id}`);
+
+        const index = waitingUsers.indexOf(socket.id);
+        if(index !== -1) waitingUsers.splice(index,1);
+
+        const rooms = io.sockets.adapter.sids.get(socket.id);
+        if(rooms){
+            rooms.forEach((roomId)=>{
+                socket.to(roomId).emit("partner_disconnected")
+            })
+        }
+
+    })
+
+})
+
+
+server.listen(PORT,()=>{
     console.log("comm service is working")
 })
