380b8f9eead11d5a3189a52ce858fe454c40ecde
UnknownCoding <aaqibakeel75@gmail.com>
2025-08-27 02:29:59 +0400

    fixing ui

--- frontend/src/app/(protected)/startups/page.tsx
+++ frontend/src/app/(protected)/startups/page.tsx
@@ -16,8 +16,8 @@ const page = () => {
             <div className='flex-1 flex flex-col p-2'>
                 <div className='font-semibold text-3xl p-5 flex items-start justify-between'>
                     <h1>Startup</h1>
-                    <button className=' text-base outline-none cursor-pointer border-[1px] border-[#3d6cfb] ml-auto mr-10 bg-blue-700 text-white gap-2 font-bold rounded-sm py-[5px] px-[10px] w-fit flex items-center  '>
-                        Save <Save className='w-6 h-6'/>
+                    <button className='text-base outline-none cursor-pointer border border-[#c9c9c9] ml-auto mr-10 bg-white text-[#4e4e4e] gap-2 font-medium rounded-md py-1 px-4 w-fit flex items-center transition-all duration-300 ease-linear hover:border-[#7d7d7d] hover:bg-gray-50 hover:shadow-sm'>
+                        Save <Save className='w-5 h-5'/>
                     </button>
                 </div>
                 <div className='flex-1 p-5 h-full gap-16 overflow-y-auto flex flex-col max-h-[552px]'>
@@ -38,21 +38,21 @@ const page = () => {
                     <div className='flex-1 flex flex-col items-center gap-16 py-5 justify-start '>
                         <div className='flex items-start gap-16  min-w-3xl '>
                             <div className='flex flex-col max-w-[200px] w-full'>
-                                <h2 className='text-[20px]'>Name</h2>
+                                <h2 className='text-[20px] font-semibold'>Name</h2>
                                 <p className='text-[#848484] text-[15px] '>Name of your startup</p>
                             </div>
                             <input className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Startup Name'/>
                         </div>
                         <div className='flex items-start gap-16  min-w-3xl '>
                             <div className='flex flex-col max-w-[200px] w-full'>
-                                <h2 className='text-[20px]'>Description</h2>
+                                <h2 className='text-[20px] font-semibold'>Description</h2>
                                 <p className='text-[#848484] text-[15px] '>Detailed description of your startup</p>
                             </div>
                             <textarea rows={3} className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Startup Description'/>
                         </div>
                         <div className='flex items-start gap-16  min-w-3xl '>
                             <div className='flex flex-col max-w-[200px] w-full'>
-                                <h2 className='text-[20px]'>Domain Name</h2>
+                                <h2 className='text-[20px] font-semibold'>Domain Name</h2>
                                 <p className='text-[#848484] text-[15px] '>Your websites domain name</p>
                             </div>
                             <input className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Domain Name'/>
--- frontend/src/components/protectedRoutes/integrations/IntegrationCard.tsx
+++ frontend/src/components/protectedRoutes/integrations/IntegrationCard.tsx
@@ -1,4 +1,5 @@
 "use client"
+import { MoveUpRight } from 'lucide-react';
 import Image from 'next/image'
 import React from 'react'
 
@@ -10,21 +11,23 @@ interface IntegrationCardI{
 
 const IntegrationCard = ({name,src,desc}:IntegrationCardI) => {
     return (
-        <div className='h-full max-h-[200px] flex flex-col  w-full max-w-[480px] border-[2px] border-[#c8c8c8] rounded-xl p-2 py-3'>
+        <div className='h-full max-h-[200px] flex flex-col  w-full max-w-[480px] border border-[#c9c9c9] rounded-md p-4'>
             <div className='flex-1 flex flex-row gap-6'>
-                <div className=' rounded-xl max-h-[70px] w-full h-full max-w-[70px] relative border-2 border-[#c8c8c8]'>
+                <div className=' rounded-lg max-h-[70px] w-full h-full max-w-[70px] relative border border-[#c9c9c8]'>
                     <Image alt='' src={`/assets/${src}`} className='object-contain p-2' fill/>
                 </div>
+                {/* TODO: Add a max-h over here to account for the overflow-y-auto */}
                 <div className='flex flex-col items-start gap-1 overflow-y-auto'>
                     <h2 className='font-bold text-[18px]'>{name}</h2>
-                    <p className='text-[14px] font-semibold text-[#646464]'>
+                    <p className='text-[14px] font-semibold text-[#838383]'>
                         {desc}
                     </p>
                 </div>
             </div>  
             <div className='w-full flex items-center justify-end px-4 py-2'>
-                <button onClick={() => {}} className=' font-bold cursor-pointer px-2 py-[1px] border-2 border-[#050db5] text-[#ffffff] outline-none rounded-md bg-[#1e4efa]'>
-                    Connect
+                <button onClick={() => {}} className='text-base font-medium cursor-pointer px-3 py-[4px] text-[#4e4e4e] outline-none rounded-md bg-white border border-[#c9c9c9] transition-all duration-300 ease-linear hover:border-[#7d7d7d] hover:bg-gray-50 hover:shadow-sm flex items-center gap-2 group'>
+                    <span>Connect</span>
+                    <MoveUpRight className='w-4 h-4 transition-transform duration-300 ease-linear group-hover:translate-x-1 group-hover:-translate-y-1'/>
                 </button>
             </div>
         </div>
