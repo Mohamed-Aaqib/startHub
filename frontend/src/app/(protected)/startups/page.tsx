f38d560dfd3b64c38ccfbd84d50637a1c15a0e67
UnknownCoding <aaqibakeel75@gmail.com>
2025-08-27 00:54:45 +0400

    fixed startup ui (temporary)

--- frontend/src/app/(protected)/startups/page.tsx
+++ frontend/src/app/(protected)/startups/page.tsx
@@ -1,4 +1,4 @@
-import { ImageUp } from 'lucide-react';
+import { ImageUp, Save } from 'lucide-react';
 import { Noto_Sans } from 'next/font/google';
 import Image from 'next/image';
 import React from 'react'
@@ -14,10 +14,13 @@ const page = () => {
     return (
         <div className={`flex flex-col h-full w-full ${notoSansFont.className}`}>
             <div className='flex-1 flex flex-col p-2'>
-                <div className='font-semibold text-3xl p-5'>
+                <div className='font-semibold text-3xl p-5 flex items-start justify-between'>
                     <h1>Startup</h1>
+                    <button className=' text-base outline-none cursor-pointer border-[1px] border-[#3d6cfb] ml-auto mr-10 bg-blue-700 text-white gap-2 font-bold rounded-sm py-[5px] px-[10px] w-fit flex items-center  '>
+                        Save <Save className='w-6 h-6'/>
+                    </button>
                 </div>
-                <div className='flex-1 p-5 h-full overflow-y-auto flex flex-col bg-amber-700 max-h-[552px]'>
+                <div className='flex-1 p-5 h-full gap-16 overflow-y-auto flex flex-col max-h-[552px]'>
                     <div className='w-full flex justify-center items-center'>
                         <div className='w-[150px] h-[150px] relative rounded-full cursor-pointer'>
                             {/* <Image 
@@ -32,22 +35,28 @@ const page = () => {
                         </div>
                         <input hidden type=''/>
                     </div>
-                    <div className='flex-1 flex flex-col items-center gap-10 py-5 justify-start '>
-                        <div className='flex items-start gap-16  min-w-2xl '>
+                    <div className='flex-1 flex flex-col items-center gap-16 py-5 justify-start '>
+                        <div className='flex items-start gap-16  min-w-3xl '>
                             <div className='flex flex-col max-w-[200px] w-full'>
                                 <h2 className='text-[20px]'>Name</h2>
-                                <p className='text-[#848484] text-[15px] '>This would be your websites name</p>
+                                <p className='text-[#848484] text-[15px] '>Name of your startup</p>
                             </div>
-                            <input className='outline-none px-3 py-2 border-[1px] w-full border-[#474747] rounded-md' placeholder='Startup Name'/>
+                            <input className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Startup Name'/>
                         </div>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup domain'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup name'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup domain'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup name'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup domain'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup name'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup domain'/>
-                        <input className='outline-none px-3 py-[2px]' placeholder='enter startup name'/>
+                        <div className='flex items-start gap-16  min-w-3xl '>
+                            <div className='flex flex-col max-w-[200px] w-full'>
+                                <h2 className='text-[20px]'>Description</h2>
+                                <p className='text-[#848484] text-[15px] '>Detailed description of your startup</p>
+                            </div>
+                            <textarea rows={3} className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Startup Description'/>
+                        </div>
+                        <div className='flex items-start gap-16  min-w-3xl '>
+                            <div className='flex flex-col max-w-[200px] w-full'>
+                                <h2 className='text-[20px]'>Domain Name</h2>
+                                <p className='text-[#848484] text-[15px] '>Your websites domain name</p>
+                            </div>
+                            <input className='outline-none px-3 py-2 border-[1px] w-full transition-colors duration-300 ease-linear focus:border-[#7d7d7d] border-[#c9c9c9] rounded-md' placeholder='Domain Name'/>
+                        </div>
                     </div>
                 </div>
             </div>
